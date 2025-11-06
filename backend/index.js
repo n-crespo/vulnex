b2c13ea7e30c766b8171dfe5fc584346a09b2335
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-11-06 00:45:15 -0800

    feat: connect to mongo database

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const mongoose = require("mongoose");
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+const app = express();
+
+const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("hello from node API");
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://ncrespo:nYeImnG0mGZqgfcg@db.0nxw0db.mongodb.net/Node-API?appName=db",
+  )
+  .then(() => {
+    console.log("connected to the db!");
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
