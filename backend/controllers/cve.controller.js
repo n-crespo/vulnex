ce3c354bf312826e9d0b384c8747103b0018a38c
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-11-07 22:30:05 -0800

    chore: organize methods in CRUD order

--- backend/controllers/cve.controller.js
+++ backend/controllers/cve.controller.js
@@ -1,6 +1,17 @@
 const { get } = require("mongoose");
 const CVE = require("../models/cve.model.js");
 
+// add some arbitrary CVE to the database
+const createCVE = async (req, res) => {
+  try {
+    const cve = await CVE.create(req.body);
+    console.log(req.body);
+    res.status(200).json(cve);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 // get all CVEs
 const getCVEs = async (req, res) => {
   try {
@@ -23,34 +34,7 @@ const getCVE = async (req, res) => {
   }
 };
 
-// delete a CVE by its ID
-const deleteCVE = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const cve = await CVE.findByIdAndDelete(id);
-
-    if (!cve) {
-      // cve doesn't exist
-      return res.status(404).json({ message: "Product not found" });
-    }
-
-    res.status(200).json({ message: "CVE deleted successfully" });
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-};
-
-// add some arbitrary CVE to the database
-const createCVE = async (req, res) => {
-  try {
-    const cve = await CVE.create(req.body);
-    console.log(req.body);
-    res.status(200).json(cve);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
+// update a CVE by
 const updateCVE = async (req, res) => {
   try {
     const { id } = req.params;
@@ -64,6 +48,23 @@ const updateCVE = async (req, res) => {
     // check for the updated CVE
     const updatedCVE = await CVE.findById(id);
     res.status(200).json(updatedCVE);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// delete a CVE by its ID
+const deleteCVE = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const cve = await CVE.findByIdAndDelete(id);
+
+    if (!cve) {
+      // cve doesn't exist
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "CVE deleted successfully" });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- backend/routes/cve.route.js
+++ backend/routes/cve.route.js
@@ -8,11 +8,19 @@ const {
   deleteCVE,
 } = require("../controllers/cve.controller.js");
 
-// root URL = /api/cves
-router.get("/", getCVEs);
-router.get("/:id", getCVE);
+// note: root URL ("/") = /api/cves
+
+// read CVEs from the db
+router.get("/", getCVEs); // all CVEs
+router.get("/:id", getCVE); // read by ID
+
+// create a CVE
 router.post("/", createCVE);
+
+// update a CVE by ID
 router.put("/:id", updateCVE);
+
+// delete a CVE by ID
 router.delete("/:id", deleteCVE);
 
 module.exports = router;
