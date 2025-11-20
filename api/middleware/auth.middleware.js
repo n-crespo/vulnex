3a7dd8c16993cd5ad2643dc981bf522fa50bdb17
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-11-19 23:44:54 -0800

    feat(api): require API key for write access

---
+++ api/middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+// note: since requests are sent via HTTPS the API key is secure
+const API_SECRET_KEY = process.env.API_SECRET_KEY;
+
+const authenticateWriteAccess = (req, res, next) => {
+  // check for api key in 'x-api-key'
+  const apiKey = req.header("x-api-key");
+
+  if (!apiKey) {
+    return res.status(401).json({
+      message: "Access Denied: No API Key provided in the x-api-key header.",
+    });
+  }
+
+  if (apiKey !== API_SECRET_KEY) {
+    return res.status(403).json({
+      message: "Access Forbidden: Invalid API Key.",
+    });
+  }
+
+  next();
+};
+
+export default authenticateWriteAccess;
--- api/routes/cve.route.js
+++ api/routes/cve.route.js
@@ -7,21 +7,28 @@ import {
   deleteCVE,
 } from "../controllers/cve.controller.js";
 
+// import auth middleware
+import authenticateWriteAccess from "../middleware/auth.middleware.js";
+
 const router = Router();
 
 // note: root URL ("/") = /api/cves
+
+// --- public read access (get) ---
 
 // read CVEs from the db
 router.get("/", getCVEs); // all CVEs
 router.get("/:id", getCVE); // read by ID
 
+// --- protected write access (post/put/delete) ---
+
 // create a CVE
-router.post("/", createCVE);
+router.post("/", authenticateWriteAccess, createCVE);
 
 // update a CVE by ID
-router.put("/:id", updateCVE);
+router.put("/:id", authenticateWriteAccess, updateCVE);
 
 // delete a CVE by ID
-router.delete("/:id", deleteCVE);
+router.delete("/:id", authenticateWriteAccess, deleteCVE);
 
 export default router;
