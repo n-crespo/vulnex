55c44c07f73620633d2afde118c6ae8350a4df22
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-11-20 01:47:52 -0800

    docs: add note about how indexing affects read/write speeds

--- api/models/cve.model.js
+++ api/models/cve.model.js
@@ -1,5 +1,9 @@
 import { Schema, model } from "mongoose";
 
+// NOTE: adding 'index: true' will speed up read queries like finding/filtering
+// CVEs but will slow down write operations since MongoDB has to make a new
+// index for every change. This is an acceptable tradeoff since we will be
+// finding/filtering much more often than writing to the CVE database.
 const CVESchema = Schema(
   {
     // primary unique key
