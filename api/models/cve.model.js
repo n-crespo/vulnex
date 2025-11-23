bcd82546807260a1dcdfc67e88d30f5f4558899e
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-11-22 18:39:21 -0800

    feat(db): add regex validators to data model CVE ID and Severity Score

--- api/models/cve.model.js
+++ api/models/cve.model.js
@@ -1,6 +1,14 @@
 import { Schema, model } from "mongoose";
 
-// NOTE: adding 'index: true' will speed up read queries like finding/filtering
+// This regex covers the official CVE format:
+// 1. Starts with 'CVE' or 'VUL' (case-insensitive flag 'i')
+// 2. hyphen
+// 3. Followed by a 4-digit year
+// 4. hyphen
+// 5. at least one digit, generally 4 or more
+const cveIdRegex = /^(CVE|VUL|TEST)-\d{4}-\d{4,}$/i;
+
+// PERF adding 'index: true' will speed up read queries like finding/filtering
 // CVEs but will slow down write operations since MongoDB has to make a new
 // index for every change. This is an acceptable tradeoff since we will be
 // finding/filtering much more often than writing to the CVE database.
@@ -12,6 +20,9 @@ const CVESchema = Schema(
       required: true,
       unique: true, // prevent duplicates
       index: true,
+      match: [cveIdRegex, "cveId must be in the format CVE-YYYY-NNNN+"], // regex validator
+      trim: true,
+      uppercase: true,
     },
     // TODO: turn these dates from ISO time stamp format into Date objects for
     // proper indexing.
@@ -27,27 +38,43 @@ const CVESchema = Schema(
     },
     status: {
       type: String,
+      required: true,
       index: true,
     },
     description: {
       type: String,
+      required: true,
       text: true, // creates a text index for keyword searching
     },
     baseSeverityScore: {
       type: Number,
-      index: true, // allow for fast querying by severity
+      required: true,
+      min: [1, "Base severity score must be at least 1."],
+      max: [10, "Base severity score cannot exceed 10."],
+      validate: {
+        validator: function (v) {
+          // mongoose will coerce types AFTER this function passes. this
+          // disallows true --> 1 from working
+          return typeof v === "number" && !isNaN(v);
+        },
+        message: (props) =>
+          `${props.value} is not a valid number for base severity score!`,
+      },
     },
     isVulnerable: {
       type: Boolean,
+      required: true,
       index: true,
     },
     // TODO: split up cpeId into product/version. currently not indexed.
     cpeId: {
       type: String,
+      required: true,
     },
   },
   {
     timestamps: true,
+    strict: true, // don't allow fields that aren't in the schema
   },
 );
 
