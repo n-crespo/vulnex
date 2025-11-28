7b8c142c93000b23595f9d5940b635d599eca1ef
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-11-28 02:21:02 -0800

    style(api): improve logging formatting

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import express, { json } from "express";
-import cors from "cors"; // needed so the github pages can talk to Azure on the backend 
+import cors from "cors"; // needed so the github pages can talk to Azure on the backend
 import { connect } from "mongoose";
 import cveRoute from "./routes/cve.route.js";
 import userRoute from "./routes/userRegisterLogin.route.js";
@@ -10,13 +10,12 @@ const port = process.env.PORT || 3000;
 const MONGO_DB_URI = process.env.MONGO_DB_URI;
 
 // cors middleware is needed for "cross-origin requests"/frontend-backend comms
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://n-crespo.github.io"
-  ],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://n-crespo.github.io"],
+    credentials: true,
+  }),
+);
 
 // middleware to support sending json
 app.use(json({ limit: "50mb" }));
@@ -26,13 +25,13 @@ app.use("/api/cves", cveRoute);
 app.use("/api/users", userRoute);
 
 // visible at root
-app.get("/", (req, res) => {
-  console.log("[GET] Fetching root /\n--------------------");
+app.get("/", (_, res) => {
+  console.log("--- [GET] Fetching root /");
   res.send("hello from node API");
 });
 
 const stableOptions = {
-  serverApi: {version: '1', strict: true, depreciationErrors: true}
+  serverApi: { version: "1", strict: true, depreciationErrors: true },
 };
 
 // connection to real database
