dd365afc5e422794312341aaef671f4a85ce9241
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-11-28 02:21:02 -0800

    style(api): improve logging formatting

--- api/middleware/auth.middleware.js
+++ api/middleware/auth.middleware.js
@@ -6,20 +6,20 @@ const authenticateWriteAccess = (req, res, next) => {
   const apiKey = req.header("x-api-key");
 
   if (!apiKey) {
-    console.log("ACCESS DENIED, no api key provided.\n--------------------");
+    console.log("--- ACCESS DENIED, no api key provided.");
     return res.status(401).json({
       message: "Access Denied: No API Key provided in the x-api-key header.",
     });
   }
 
   if (apiKey !== API_SECRET_KEY) {
-    console.log(`Wrong API Key: ${apiKey}\n--------------------`);
+    console.log(`--- Wrong API Key: ${apiKey}`);
     return res.status(403).json({
       message: "Access Forbidden: Invalid API Key.",
     });
   }
 
-  console.log(`API Key is correct!`);
+  // console.log(`API Key is correct!`);
   next();
 };
 
